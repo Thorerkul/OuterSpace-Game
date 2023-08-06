534cb84bb9b2eff291e61e5a23261e92fda90325
T - TA <thoukkol@gmail.com>
2023-08-06 12:53:43 +0200

    Reverted all changes related to pixelart shader

--- OuterSpaceProject/Assets/CameraController.cs
+++ OuterSpaceProject/Assets/CameraController.cs
@@ -6,9 +6,18 @@ public class CameraController : MonoBehaviour
 {
     public Transform playerpos;
     public Vector3 offset;
+    public Camera cam;
+    public float camzoomSpeed;
 
     private void Update()
     {
+        float vertical = Input.GetAxisRaw("DpadVertical");
+        cam.orthographicSize += vertical * camzoomSpeed * Time.deltaTime;
+        if (cam.orthographicSize < 1)
+        {
+            cam.orthographicSize = 1;
+        }
+
         transform.position = new Vector3(playerpos.position.x + offset.x, playerpos.position.y + offset.y, playerpos.position.z + offset.z);
     }
 }
--- OuterSpaceProject/Assets/World.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class World
-{
-    public Vector3 Rotation = new Vector3 (-45, 0, 0);
-}

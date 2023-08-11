e932c8462d9c8d3319b6634c62bcf00ae4c62502
T - TA <thoukkol@gmail.com>
2023-08-11 19:48:25 +0200

    MENU

--- OuterSpaceProject/Assets/Audio/VolumeSettings.cs
+++ OuterSpaceProject/Assets/Audio/VolumeSettings.cs
@@ -9,15 +9,36 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
 
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider sfxSlider;
+
     private void Start()
     {
         if (PlayerPrefs.HasKey("musicVolume"))
         {
-            LoadVolume();
+            LoadMusicVolume();
         }
         else
         {
             SetMusicVolume();
+        }
+
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            LoadMasterVolume();
+        }
+        else
+        {
+            SetMasterVolume();
+        }
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadSFXVolume();
+        }
+        else
+        {
+            SetsfxVolume();
         }
     }
 
@@ -28,15 +49,45 @@ public class VolumeSettings : MonoBehaviour
         PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
-    private void LoadVolume()
+    public void SetMasterVolume()
+    {
+        float volume = masterSlider.value;
+        myMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetsfxVolume()
+    {
+        float volume = sfxSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadMusicVolume()
     {
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
 
         SetMusicVolume();
     }
 
+    private void LoadMasterVolume()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+
+        SetMasterVolume();
+    }
+
+    private void LoadSFXVolume()
+    {
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetsfxVolume();
+    }
+
     private void Update()
     {
         SetMusicVolume();
+        SetMasterVolume();
+        SetsfxVolume();
     }
 }

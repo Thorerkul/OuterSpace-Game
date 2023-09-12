39276163c5ab68fa34c30952fb035b54afbb4267
T - TA <thoukkol@gmail.com>
2023-09-12 17:28:22 +0200

    h

--- OuterSpaceProject/Assets/CombatFunctions.cs
+++ OuterSpaceProject/Assets/CombatFunctions.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class CombatFunctions : MonoBehaviour
 {
+    private float timer = 1;
+    private float time = 1;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0 )
+        {
+            timer = time;
+
+            System.GC.Collect();
+            System.GC.WaitForPendingFinalizers();
+        }
+    }
+
     public float damageCalculator(float  damage, float defence, float ap)
     {
         float x;
